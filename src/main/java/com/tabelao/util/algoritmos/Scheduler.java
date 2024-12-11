7957b84a8417b145877dbbc697d361480347a9e4
Bruno Anastacio <82920706+samiaanastacio@users.noreply.github.com>
2024-12-10 21:00:25 -0300

    Recebe uma lista de datas globalmente bloqueadas e evita agendamento de jogos nestas datas

--- src/main/java/com/tabelao/util/algoritmos/Scheduler.java
+++ src/main/java/com/tabelao/util/algoritmos/Scheduler.java
@@ -20,15 +20,25 @@ public class Scheduler {
         int[] semanaOriginal = request.semana(); // Configuração semanal original
         int[] semana = semanaOriginal.clone(); // Clonar para controlar a capacidade atual
         DayOfWeek dayOfWeek = dia.getDayOfWeek();
-        int diaDaSemana = dayOfWeek.getValue()%7; // Índice do vetor (domingo = 0, segunda = 1, ..., sábado = 6)
+        int diaDaSemana = dayOfWeek.getValue() % 7; // Índice do vetor (domingo = 0, segunda = 1, ..., sábado = 6)
         Stack<LocalDate> datasBloqueadas = Arrays.stream(request.datasBloqueadas())
                 .sorted(Collections.reverseOrder()) // Inverte a ordem
                 .collect(Stack::new, Stack::push, Stack::addAll);
-        LocalDate proximaDataBloqueada = datasBloqueadas.pop(); //memoria de datas bloqueadas
+
+        LocalDate proximaDataBloqueada = datasBloqueadas.isEmpty() ? null : datasBloqueadas.pop(); // Memória de datas bloqueadas
 
         for (Jogo jogo : jogos) {
             // Procurar o próximo dia disponível
-            while (semana[diaDaSemana] == 0 && dia != proximaDataBloqueada) {
+            while (semana[diaDaSemana] == 0 || (dia.equals(proximaDataBloqueada))) {
+                // Caso o dia esteja bloqueado, avançar para o próximo dia
+                if (dia.equals(proximaDataBloqueada)) {
+                    if (!datasBloqueadas.isEmpty()) {
+                        proximaDataBloqueada = datasBloqueadas.pop();
+                    } else {
+                        proximaDataBloqueada = null; // Não há mais datas bloqueadas
+                    }
+                }
+
                 diaDaSemana = (diaDaSemana + 1) % 7; // Avançar para o próximo dia da semana
                 dia = dia.plusDays(1); // Incrementar a data
 
@@ -42,9 +52,24 @@ public class Scheduler {
             jogo.setData(dia);
             jogosAgendados.add(jogo);
             semana[diaDaSemana]--; // Reduzir o limite de jogos para o dia atual
-
         }
+
         return jogosAgendados;
+    }
+
+
+    public void somarData(LocalDate dia, LocalDate proximaDataBloqueada, int diaDaSemana, int [] semana, int [] semanaOriginal) {
+//       do{
+//
+//       } while(dia == proximaDataBloqueada);
+
+        diaDaSemana = (diaDaSemana + 1) % 7; // Avançar para o próximo dia da semana
+        dia = dia.plusDays(1); // Incrementar a data
+
+        // Se voltamos ao domingo, reinicia a capacidade semanal
+        if (diaDaSemana == 0) {
+            semana = semanaOriginal.clone(); // Reinicia os limites semanais
+        }
     }
 
 
